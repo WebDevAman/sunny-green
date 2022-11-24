9e2b324a51df6f73a06ca4427631ad0db7a49e35
Aman Pandey <amanpandeyfiverr@gmail.com>
2022-11-24 16:14:36 +0530

    fixes

--- src/components/pages/statements/StatementsPage.jsx
+++ src/components/pages/statements/StatementsPage.jsx
@@ -7,7 +7,8 @@ const StatementsPage = () => {
         <Layout noHero img='https://www.enie.nl/wp-content/uploads/2020/10/2009_showcase_assen_enie_6_web.jpg'>
             <div className="flex px-3 flex-col items-center  pt-12 lg:pt-2 min-h-[30rem] justify-center z-10 relative gap-6">
                 <h2 className="text-2xl lg:text-5xl max-w-xl mx-auto text-center text-white font-light">
-                    Privacy statement
+                    Algemene voorwaarden
+
                 </h2>
 
             </div>
