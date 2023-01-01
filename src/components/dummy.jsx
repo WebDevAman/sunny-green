63d7eda924658cc5dabee3ddd83d472f6e024927
sadath <sadathsadu2002@gmail.com>
2023-01-01 20:58:25 +0530

    sidebar

--- src/components/dummy.jsx
+++ src/components/dummy.jsx
@@ -1,82 +1,133 @@
 export const navData = [
-    {
-        title: "PARTICULIER",
-        slug: '/prijs-berekenen-zonnepanelen',
-        submenu: [
-            // {
-            //     title: `ZonneHuren`,
-            //     slug: `/particulier/zonnepanelen-huren`,
-            // },
+  {
+    title: 'PARTICULIER',
+    slug: '/prijs-berekenen-zonnepanelen',
+    submenu: [
+      // {
+      //     title: `ZonneHuren`,
+      //     slug: `/particulier/zonnepanelen-huren`,
+      // },
 
-            {
-                title: `ZonneKopen`,
-                slug: `/particulier/zonnepanelen-kopen`,
-            },
-            // {
-            //     title: `ZonneSparen`,
-            //     slug: `/particulier/zonnesparen`,
-            // },
-            {
-                title: `Laadpaal thuis`,
-                slug: `/particulier/laadpaal-thuis`,
-            },
-        ]
-    },
-    {
-        title: "ZAKELIJK",
-        slug: '/prijs-berekenen-zonnepanelen',
-        submenu: [
-            {
-                title: `ZonneLeasen MKB`,
-                slug: `/zakelijk/zonnepanelen-leasen`,
-            },
-            // {
-            //     title: `ZonneCarport`,
-            //     slug: `/zakelijk/carport-zonnepanelen`,
-            // },
-            {
-                title: `ZonneKopen Zakelijk`,
-                slug: `/prijs-berekenen-zonnepanelen`,
-            },
-            // {
-            //     title: `De Groene WKR`,
-            //     slug: `/zakelijk/de-groene-wkr-werkgever`,
-            // },
-
-
-
-
-        ]
-    },
-    {
-        title: "OVER ONS",
-        slug: '/over-sunnygreen-nl/',
-        submenu: [
-            {
-                title: `Veelgestelde vragen`,
-                slug: `/veelgestelde-vragen-over-zonnepanelen`,
-            },
-            {
-                title: `Privacy statement`,
-                slug: `/privacy-statement-sunnygreen-nl`,
-            },
-            {
-                title: `Cookie statement`,
-                slug: `/cookie-statement-sunnygreen-nl`,
-            },
-            {
-                title: `Contact`,
-                slug: `/contact`,
-            },
-            {
-                title: `Algemene voorwaarden`,
-                slug: `/algemene-voorwaarden`,
-            },
-            {
-                title: `Onze producten`,
-                slug: `/prijs-berekenen-zonnepanelen`,
-            },
-        ]
-    },
-
+      {
+        title: `ZonneHuren`,
+        slug: `/particulier/zonnepanelen-huren/`,
+      },
+      {
+        title: `ZonneKopen`,
+        slug: `/particulier/zonnepanelen-kopen/`,
+      },
+      {
+        title: `ZonneLaden`,
+        slug: `/particulier/laadpaal-thuis/`,
+      },
+      {
+        title: `ZonneSparen`,
+        slug: `/particulier/zonnesparen/`,
+      },
+      // {
+      //     title: `ZonneSparen`,
+      //     slug: `/particulier/zonnesparen`,
+      // },
+      // {
+      //     title: `Laadpaal thuis`,
+      //     slug: `/particulier/laadpaal-thuis`,
+      // },
+    ],
+  },
+  {
+    title: 'ZAKELIJK',
+    slug: '/prijs-berekenen-zonnepanelen',
+    submenu: [
+      {
+        title: `MKB ZonneLeasen`,
+        slug: `/zakelijk/zonnepanelen-leasen/`,
+      },
+      {
+        title: `ZonneKopen`,
+        slug: `/zakelijk/zonnepanelen-kopen/`,
+      },
+      {
+        title: `Carport`,
+        slug: `/zakelijk/carport-zonnepanelen/`,
+      },
+      {
+        title: `WKR`,
+        slug: `/zakelijk/de-groene-wkr-werkgever/`,
+      },
+      //   {
+      //     title: `ZonneLeasen MKB`,
+      //     slug: `/zakelijk/zonnepanelen-leasen`,
+      //   },
+      // {
+      //     title: `ZonneCarport`,
+      //     slug: `/zakelijk/carport-zonnepanelen`,
+      // },
+      //   {
+      //     title: `ZonneKopen Zakelijk`,
+      //     slug: `/prijs-berekenen-zonnepanelen`,
+      //   },
+      // {
+      //     title: `De Groene WKR`,
+      //     slug: `/zakelijk/de-groene-wkr-werkgever`,
+      // },
+    ],
+  },
+  {
+    title: 'OVER ONS',
+    slug: '/over-sunnygreen-nl/',
+    submenu: [
+      {
+        title: `B Corp`,
+        slug: `/over-enie-nl/bcorp`,
+      },
+      {
+        title: `Producten`,
+        slug: `/over-enie-nl/onze-producten`,
+      },
+      {
+        title: `Kennisbank`,
+        slug: `/kennisbank`,
+      },
+      {
+        title: `Ons team`,
+        slug: `/over-enie-nl/ons-team/`,
+      },
+      {
+        title: `Vriendenactie`,
+        slug: `/over-enie-nl/vriendenactie/`,
+      },
+      {
+        title: `Vacatures`,
+        slug: `/veelgestelde-vragen-over-zonnepanelen`,
+      },
+      {
+        title: `Nieuws`,
+        slug: `/nieuws/`,
+      },
+      //   {
+      //     title: `Veelgestelde vragen`,
+      //     slug: `/veelgestelde-vragen-over-zonnepanelen`,
+      //   },
+      //   {
+      //     title: `Privacy statement`,
+      //     slug: `/privacy-statement-sunnygreen-nl`,
+      //   },
+      //   {
+      //     title: `Cookie statement`,
+      //     slug: `/cookie-statement-sunnygreen-nl`,
+      //   },
+      //   {
+      //     title: `Contact`,
+      //     slug: `/contact`,
+      //   },
+      //   {
+      //     title: `Algemene voorwaarden`,
+      //     slug: `/algemene-voorwaarden`,
+      //   },
+      //   {
+      //     title: `Onze producten`,
+      //     slug: `/prijs-berekenen-zonnepanelen`,
+      //   },
+    ],
+  },
 ]
